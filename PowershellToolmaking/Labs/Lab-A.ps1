b0607f2fb20e8173d9b08330279b6e08e226c449
Marco Janse <marco.janse@referit.nl>
2018-07-01 15:31:28 +0200

    LAB A expanded, chapter 8.9.1.

--- PowershellToolmaking/Labs/Lab-A.ps1
+++ PowershellToolmaking/Labs/Lab-A.ps1
@@ -11,16 +11,18 @@
 .OUTPUTS
     Output (if any)
 .NOTES
-    PowerShell Toolmaking in a Month of Lunches, 7.9.1. LAB A
-    Version 1.2
+    PowerShell Toolmaking in a Month of Lunches, LAB A
+    Version 1.3
     Last Modified on 01-07-2018
     Designed by Don Jones and Jeffrey Hicks
     Lab executed by Marco Janse
 
     Version History:
+    1.3 - Lab A expanded, chapter 8.9.1.
+          Added Verbose output and parameter attributes
     1.2 - Changed the file naming to mention Lab name
     1.1 - Changed Error log to default location
-    1.0 - LAB A, chapter 7.9.1.
+    1.0 - Lab A, chapter 7.9.1.
 
 #>
 
@@ -28,18 +30,26 @@ function Get-ComputerInfo {
     [CmdletBinding()]
     param(
         # Parameter ComputerName
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
         [string[]] $ComputerName='localhost',
 
         # Parameter ErrorLog
         [string] $ErrorLog='C:\ErrorLog.txt'
     )
 
-    BEGIN {}
+    BEGIN {
+        Write-Verbose "Running BEGIN-block.."
+        Write-Verbose "Ending BEGIN-block.."
+    }
 
     PROCESS {
+        Write-Verbose "Beginning PROCESS-block.."
 
         foreach ($Computer in $ComputerName) {
+            Write-Verbose "Processing $computer"
+            Write-Verbose 'Quering WMI using CIM cmdlets...'
             $comp = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $Computer
             $bios = Get-CimInstance -ClassName Win32_BIOS -ComputerName $Computer
             $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $Computer
@@ -59,15 +69,17 @@ function Get-ComputerInfo {
                 'ServicePackMajorVersion'=$os.ServicePackMajorVersion
             } # props
 
+            Write-Verbose "adding custom objects to PSObject for $Computer"
             $obj = New-Object -TypeName psobject -Property $props
             Write-Output $obj
 
-        } # for each
+        } # for each Computer
 
+        Write-Verbose "Ending PROCESS-block..."
     } # PROCESS
 
     END {}
 
 } # Get-ComputerInfo
 
-Get-ComputerInfo -ComputerName localhost,localhost
+'localhost','maja-lpt-01' | Get-ComputerInfo -Verbose
