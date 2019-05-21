befdbebbc66b091c10c0f496c00c7f5a7752c12a
Marco Janse <marco.janse@outlook.com>
2019-05-21 22:34:18 +0200

    1.2 - Added trace code - listing 11.5

--- PowershellToolmaking/Labs/debug.ps1
+++ PowershellToolmaking/Labs/debug.ps1
@@ -15,25 +15,33 @@
     Lab executed by Marco Janse
     
     Version History:
+    1.2 - Added trace code - listing 11.5
     1.1 - Forgot a bloody line
     1.0 - Did the initial exercise from listing 11.4
 #>
 
+[CmdletBinding()]
+param()
 $data = Import-Csv C:\Scripts\Input\data.csv
+Write-Debug -Message "Imported CSV Data"
+
 $totalqty = 0
+
 $totalsold = 0
 $totalbought = 0
 foreach ($line in $data) {
     if ($line.transaction -eq 'buy') {
-        # buy transaction (we sold)
+        Write-Debug -Message "ENDED BUY transaction (we sold)"
         $totalqty -= $line.qty
-        $totalsold = $line.total 
-    } 
-    else {
-        #sell transaction (we bought)
+        $totalsold = $line.total
+    } else {
         $totalqty += $line.qty
         $totalbought = $line.total 
+        Write-Debug -Message "ENDED SELL transaction (we bought)"
     }
 }
+
+Write-Debug -Message "OUTPUT: $totalqty,$totalbought,$totalsold,$($totalbought-$totalsold)"
+
 "totalqty,totalbought,totalsold,totalamt" | Out-File C:\Scripts\Output\summary.csv
 "$totalqty,$totalbought,$totalsold,$($totalbought-$totalsold)" | Out-File C:\Scripts\Output\summary.csv -Append
