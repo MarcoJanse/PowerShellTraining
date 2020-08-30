3f8e73118fc557ad65b2db60b94468b53ca4dab3
Marco Janse <marco.janse@outlook.com>
2020-08-30 21:03:04 +0200

    added second example

--- Examples/Example-Switch.ps1
+++ Examples/Example-Switch.ps1
@@ -6,17 +6,43 @@
 
     More info: help about_switch
 .NOTES
-    version 1.0
-    2018-06-24
+    version 1.1
+    2020-08-39
     By Marco Janse
 
     Version History
+    1.1 added seconds example
     1.0 Created example
 #>
 
+# Example 1
 switch ((Get-CimInstance -ClassName Win32_ComputerSystem).AdminPasswordStatus){
     0 {"Disabled"}
     1 {"Enabled"}
     2 {"Not Implemented"}
     3 {"Unknown"}
 }
+
+# Example 2
+
+$x = "d1234"
+
+switch -Wildcard ($x)
+    {
+        "*1*" {"Contains 1"}
+        "*5*" {"Contains 5"}
+        "d*" {"Starts with 'd'"}
+        default {"no matches"}
+    }
+
+# Example 2.1
+
+$x = "1 of 5 dying worms"
+
+switch -Wildcard ($x)
+    {
+        "*1*" {"Contains 1"}
+        "*5*" {"Contains 5"}
+        "d*" {"Starts with 'd'"}
+        default {"no matches"}
+    }
