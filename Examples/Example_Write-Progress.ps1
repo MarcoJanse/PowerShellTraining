21c195f173b21c024d8526196c846a06b1c9cbec
Marco Janse <marco.janse@outlook.com>
2020-08-30 20:50:22 +0200

    initial version

---
+++ Examples/Example_Write-Progress.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Write-Progress Example
+.DESCRIPTION
+    Just some examples how to use Write-Progress and building logn running commands
+.NOTES
+    version 1.0
+    2020-08-30
+    By Marco Janse
+
+    Version History:
+    1.0 initial version
+#>
+
+for ($i = 1; $i -le 1000000; $i++ ) { Write-Progress "unable to comply, building in progress..." }
